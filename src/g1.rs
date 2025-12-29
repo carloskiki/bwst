8c700dab4c9694f329798763796fa97bf3fe32d3
Charles Edward Gagnon <76854355+carloskiki@users.noreply.github.com>
2025-12-29 16:35:29 -0500

    add dependencies

--- src/g1.rs
+++ src/g1.rs
@@ -1,7 +1,33 @@
+use group::Group;
+
 use crate::bindings;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct G1Affine(bindings::blst_p1_affine);
+
+impl Group for G1Affine {
+    type Scalar;
+
+    fn try_from_rng<R: TryRngCore + ?Sized>(rng: &mut R) -> Result<Self, R::Error> {
+        todo!()
+    }
+
+    fn identity() -> Self {
+        todo!()
+    }
+
+    fn generator() -> Self {
+        todo!()
+    }
+
+    fn is_identity(&self) -> Choice {
+        todo!()
+    }
+
+    fn double(&self) -> Self {
+        todo!()
+    }
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct G1Projective(bindings::blst_p1);
