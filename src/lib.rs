b96760328e4d0ccb3b9ed4a72a7e9c38028e1f4c
Charles Edward Gagnon <76854355+carloskiki@users.noreply.github.com>
2025-12-30 17:08:17 -0500

    re-export dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,10 @@ pub mod g2;
 pub mod miller_loop;
 pub mod scalar;
 
+pub use group;
+// FIXME: This should not be re-exported
+pub use subtle;
+
 pub(crate) mod bindings {
     #![allow(non_camel_case_types)]
     #![allow(dead_code)]
